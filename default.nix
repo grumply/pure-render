2fd530f709cf82cdb02084c5e1fb5ef3bca6076d
S. M. Hickman <sean@grump.ly>
2018-05-28 14:43:32 -0500

    Add default.nix.

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ mkDerivation, base
+, pure-core, pure-dom, pure-default, pure-lifted, pure-json, pure-txt, stdenv
+, containers
+}:
+mkDerivation {
+pname = "pure-render";
+version = "0.7.0.0";
+src = ./.;
+libraryHaskellDepends = [ base pure-core pure-dom pure-default pure-lifted pure-json pure-txt stdenv containers ];
+homepage = "github.com/grumply/pure-render";
+license = stdenv.lib.licenses.bsd3;
+}
